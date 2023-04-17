961c99eb4f8c0030e542680498594739349c5ae4
Othi <mnpq.raven@gmail.com>
2023-04-17 17:50:19 +0700

    how to write documentation

--- src/type_registry.ts
+++ src/type_registry.ts
@@ -1,9 +1,11 @@
-import type { ApiEndpoint } from "$lib/server/types";
+import type { ApiEndpoint } from '$lib/server/types';
 
-export const tParseMdx: ApiEndpoint = {
+const tParseMdx: ApiEndpoint = {
   path: 'utils/parse_mdx',
   methods: ['POST'],
   description: 'Parse a mdx file decoded in base64',
-  input: '{\n  test: string\n}',
-  output: '{\n  test: string\n}'
+  input: ['test: string'],
+  output: ['test: string']
 };
+
+export default { tParseMdx };

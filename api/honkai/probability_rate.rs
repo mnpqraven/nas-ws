4f576f6f80b112df7d974ca068ccc1c9ee21c32d
Othi <mnpq.raven@gmail.com>
2023-05-26 10:24:56 +0700

    cors stuff

--- api/honkai/probability_rate.rs
+++ api/honkai/probability_rate.rs
@@ -1,4 +1,4 @@
-use axum::{http::Method, Json, extract::FromRequest};
+use axum::{extract::FromRequest, http::Method, Json};
 use nas_ws::{
     handler::{error::WorkerError, FromAxumResponse},
     routes::honkai::gacha::probability_rate,
@@ -15,10 +15,6 @@ async fn main() -> Result<(), Error> {
 
 /// INFO: https://www.reddit.com/r/Genshin_Impact/comments/kdy1ky/everyone_is_misunderstanding_soft_pity/
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
-    // if *req.method() != Method::GET {
-    //     return Ok(WorkerError::WrongMethod.into());
-    // }
-    // NOTE: uncomment if payload is used (will be eventaully)
     let payload = Json::from_request(req, &()).await;
     probability_rate(payload).await.as_axum()
 }
--- src-rust/routes/mod.rs
+++ src-rust/routes/mod.rs
@@ -1,8 +1,11 @@
 use self::{
     dotfiles::dotfiles_routes, foo::foo_routes, honkai::honkai_routes, utils::utils_routes,
 };
-use axum::{routing::get, Router};
-use tower_http::trace::TraceLayer;
+use axum::{http::Method, routing::get, Router};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 
 pub mod dotfiles;
 mod foo;
@@ -10,6 +13,10 @@ pub mod honkai;
 pub mod utils;
 
 pub fn app_router() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PUT])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(root))
         .nest("/utils", utils_routes())
@@ -17,6 +24,7 @@ pub fn app_router() -> Router {
         .nest("/dotfiles", dotfiles_routes())
         .nest("/honkai", honkai_routes())
         .layer(TraceLayer::new_for_http())
+        .layer(CorsLayer::permissive())
 }
 
 async fn root() -> &'static str {

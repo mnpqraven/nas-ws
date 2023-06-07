7a08674729308bdb5d012f9478ce0d19ac1b99fc
Othi <mnpq.raven@gmail.com>
2023-06-07 17:31:01 +0700

    expand bp options, fix bp giving only 1 reward across multiple patches TODO: needs to refactor Patch module, smarter date tracking + const

--- src-rust/routes/honkai/types.rs
+++ src-rust/routes/honkai/types.rs
@@ -1,20 +1,47 @@
+use super::{
+    jade_estimate::{get_current_patch_boundaries, get_date_differences},
+    utils::patch_date::PatchList,
+};
 use crate::handler::error::WorkerError;
 use crate::handler::FromAxumResponse;
 use axum::Json;
-use chrono::{DateTime, Datelike, Duration, TimeZone, Timelike, Utc};
+use chrono::{DateTime, Datelike, Duration, TimeZone, Utc};
 use response_derive::JsonResponse;
 use serde::{Deserialize, Serialize};
 use std::path::Path;
 use vercel_runtime::{Body, Response, StatusCode};
 
-use super::jade_estimate::{get_current_patch_boundaries, get_date_differences};
-
 #[derive(Serialize, Deserialize, JsonResponse, Clone)]
-pub struct Rewards {
+pub struct JadeEstimateResponse {
     pub sources: Vec<RewardSource>,
     pub total_jades: i32,
     pub rolls: i32,
     pub days: i64,
+}
+
+impl From<EstimateCfg> for JadeEstimateResponse {
+    fn from(cfg: EstimateCfg) -> Self {
+        let rewards = RewardSource::compile_sources(&cfg);
+        let (diff_days, _) = get_date_differences(&cfg.server, cfg.to_date_time());
+
+        let mut total_jades: i32 = rewards.iter().map(|e| e.jades_amount.unwrap_or(0)).sum();
+        let reward_rolls: i32 = rewards.iter().map(|e| e.rolls_amount.unwrap_or(0)).sum();
+
+        if let Some(current_jades) = cfg.current_jades {
+            total_jades += current_jades;
+        }
+        let mut total_rolls = (total_jades / 160) + reward_rolls;
+        if let Some(current_rolls) = cfg.current_rolls {
+            total_rolls += current_rolls;
+        }
+
+        Self {
+            total_jades,
+            rolls: total_rolls,
+            days: diff_days.try_into().unwrap(),
+            sources: rewards,
+        }
+    }
 }
 
 #[derive(Deserialize, Clone, Debug)]
@@ -23,7 +50,7 @@ pub struct EstimateCfg {
     pub server: Server,
     pub until_date: SimpleDate,
     pub rail_pass: RailPassCfg,
-    pub battle_pass: bool,
+    pub battle_pass: BattlePassOption,
     pub eq: EqTier,
     pub current_rolls: Option<i32>,
     pub current_jades: Option<i32>,
@@ -62,64 +89,158 @@ pub enum RewardSourceType {
     OneTime,
 }
 
+#[derive(Debug, Deserialize, Clone, Copy)]
+pub enum BattlePassOption {
+    None,
+    Basic,
+    Premium,
+}
+
 impl RewardSource {
-    fn new_jade(
-        source: impl Into<String>,
-        calc_fn: impl Fn() -> i32,
-        source_type: RewardSourceType,
-    ) -> Self {
-        Self {
-            source: source.into(),
-            jades_amount: Some(calc_fn()),
-            rolls_amount: None,
-            source_type,
-        }
-    }
-
-    fn new_roll(
-        source: impl Into<String>,
-        calc_fn: impl Fn() -> i32,
-        source_type: RewardSourceType,
-    ) -> Self {
-        Self {
-            source: source.into(),
-            jades_amount: None,
-            rolls_amount: Some(calc_fn()),
-            source_type,
-        }
-    }
-
     fn compile_sources(cfg: &EstimateCfg) -> Vec<Self> {
         let dt_to = cfg.to_date_time();
         let (diff_days, _) = get_date_differences(&cfg.server, dt_to);
 
-        let daily = RewardSourceType::Daily;
-        let weekly = RewardSourceType::Weekly;
-        let monthly = RewardSourceType::Monthly;
-        let patch_long = RewardSourceType::WholePatch;
-        let patch_half = RewardSourceType::HalfPatch;
-
-        let su = || Rewards::get_su_jades(&cfg.eq, dt_to);
-        let bp = || Rewards::get_bp_jades(cfg.battle_pass);
-        let rail_pass = || Rewards::get_rail_pass_jades(&cfg.rail_pass, diff_days);
-        let daily_mission = || Rewards::get_daily_missions(diff_days);
-        let daily_text = || Rewards::get_daily_text(diff_days);
-        let lab_checkin = || Rewards::get_checkin_jades(dt_to);
-        let char_trial = || {
-            Rewards::get_character_trial_jades(get_current_patch_boundaries(Utc::now()).0, dt_to)
-        };
-        let ember_trade = || Rewards::get_monthly_ember_rolls(dt_to);
+        let src_su = Self::src_su(&cfg.eq, dt_to);
+        let src_bp = Self::src_bp(cfg.battle_pass, dt_to);
+        let src_rail_pass = Self::src_rail_pass(&cfg.rail_pass, diff_days);
+        let src_daily_mission = Self::src_daily_mission(diff_days);
+        let src_daily_text = Self::src_daily_text(diff_days);
+        let src_hoyolab_checkin = Self::src_hoyolab_checkin(dt_to);
+        let src_char_trial =
+            Self::src_char_trial(get_current_patch_boundaries(Utc::now()).0, dt_to);
+        let src_ember_trade = Self::src_ember_trade(dt_to);
 
         vec![
-            Self::new_jade("Simulated Universe", su, weekly),
-            Self::new_jade("Battle Pass", bp, patch_long),
-            Self::new_jade("Rail Pass", rail_pass, monthly),
-            Self::new_jade("Daily missions", daily_mission, daily),
-            Self::new_jade("Daily text messages", daily_text, daily),
-            Self::new_jade("HoyoLab Check-in", lab_checkin, monthly),
-            Self::new_jade("Character Trials", char_trial, patch_half),
-            Self::new_roll("Monthly ember exchange", ember_trade, monthly),
+            src_su,
+            src_bp,
+            src_rail_pass,
+            src_daily_mission,
+            src_daily_text,
+            src_hoyolab_checkin,
+            src_char_trial,
+            src_ember_trade,
         ]
+    }
+
+    fn src_bp(bp_config: BattlePassOption, dt_to: DateTime<Utc>) -> Self {
+        let patches = PatchList::patches_passed_number(dt_to) as i32;
+        let (jades_amount, rolls_amount) = match bp_config {
+            BattlePassOption::None => (None, None),
+            BattlePassOption::Basic => (Some((680 + 680) * patches), None),
+            BattlePassOption::Premium => (Some((880 + 680) * patches), Some(4 * patches)),
+        };
+        Self {
+            source: "Battle Pass".into(),
+            jades_amount,
+            rolls_amount,
+            source_type: RewardSourceType::Weekly,
+        }
+    }
+
+    fn src_su(eq_tier: &EqTier, until_date: DateTime<Utc>) -> Self {
+        let per_weeks = match eq_tier {
+            EqTier::Zero | EqTier::One => 75,
+            EqTier::Two => 105,
+            EqTier::Three => 135,
+            EqTier::Four => 165,
+            EqTier::Five => 195,
+            // WARN: NEEDS CONFIRM
+            EqTier::Six => 225,
+        };
+        let mut amount = 0;
+        for date in DateRange(Utc::now(), until_date) {
+            if let chrono::Weekday::Mon = date.weekday() {
+                amount += per_weeks;
+            }
+        }
+        Self {
+            source: "Simulated Universe".into(),
+            jades_amount: Some(amount),
+            rolls_amount: None,
+            source_type: RewardSourceType::Weekly,
+        }
+    }
+
+    fn src_daily_mission(days: u32) -> Self {
+        let jades = (60 * days).try_into().unwrap();
+        Self {
+            source: "Daily missions".into(),
+            jades_amount: Some(jades),
+            rolls_amount: None,
+            source_type: RewardSourceType::Daily,
+        }
+    }
+
+    fn src_daily_text(days: u32) -> Self {
+        let jades = (days * 5).try_into().unwrap();
+        Self {
+            source: "Daily text messages".into(),
+            jades_amount: Some(jades),
+            rolls_amount: None,
+            source_type: RewardSourceType::Daily,
+        }
+    }
+
+    pub fn src_rail_pass(cfg: &RailPassCfg, diff_days: u32) -> Self {
+        let jades = match cfg.days_left {
+            Some(days_left) if cfg.use_rail_pass => match days_left < diff_days {
+                true => 90 * diff_days as i32 + 300 * diff_days as i32 / 30,
+                false => 90 * diff_days as i32,
+            },
+            None if cfg.use_rail_pass => 90 * diff_days as i32,
+            _ => 0,
+        };
+        Self {
+            source: "Rail Pass".into(),
+            jades_amount: Some(jades),
+            rolls_amount: None,
+            source_type: RewardSourceType::Monthly,
+        }
+    }
+
+    fn src_hoyolab_checkin(until_date: DateTime<Utc>) -> Self {
+        let mut amount: i32 = 0;
+        for date in DateRange(Utc::now(), until_date) {
+            if let 5 | 13 | 20 = date.day() {
+                amount += 20;
+            }
+        }
+        Self {
+            source: "HoyoLab Check-in".into(),
+            jades_amount: Some(amount),
+            rolls_amount: None,
+            source_type: RewardSourceType::Monthly,
+        }
+    }
+
+    fn src_char_trial(patch_start: DateTime<Utc>, until_date: DateTime<Utc>) -> Self {
+        // rewards from both banners in patch
+        let jades = match patch_start + Duration::weeks(3) < until_date {
+            true => 40,
+            false => 20,
+        };
+        Self {
+            source: "Character Trials".into(),
+            jades_amount: Some(jades),
+            rolls_amount: None,
+            source_type: RewardSourceType::HalfPatch,
+        }
+    }
+
+    fn src_ember_trade(until_date: DateTime<Utc>) -> Self {
+        let mut amount: i32 = 0;
+        for date in DateRange(Utc::now(), until_date) {
+            if let 1 = date.day() {
+                amount += 5;
+            }
+        }
+        Self {
+            source: "Monthly ember exchange".into(),
+            jades_amount: None,
+            rolls_amount: Some(amount),
+            source_type: RewardSourceType::Monthly,
+        }
     }
 }
 
@@ -200,7 +321,7 @@ impl Default for SurveyRate {
     }
 }
 
-impl Rewards {
+impl JadeEstimateResponse {
     pub fn from_cfg(cfg: EstimateCfg) -> Self {
         let rewards = RewardSource::compile_sources(&cfg);
         let (diff_days, _) = get_date_differences(&cfg.server, cfg.to_date_time());
@@ -222,79 +343,6 @@ impl Rewards {
             days: diff_days.try_into().unwrap(),
             sources: rewards,
         }
-    }
-
-    fn get_daily_missions(days: u32) -> i32 {
-        (60 * days).try_into().unwrap()
-    }
-
-    fn get_daily_text(days: u32) -> i32 {
-        (days * 5).try_into().unwrap()
-    }
-
-    fn get_su_jades(eq_tier: &EqTier, until_date: DateTime<Utc>) -> i32 {
-        let per_weeks = match eq_tier {
-            EqTier::Zero | EqTier::One => 75,
-            EqTier::Two => 105,
-            EqTier::Three => 135,
-            EqTier::Four => 165,
-            EqTier::Five => 195,
-            // WARN: NEEDS CONFIRM
-            EqTier::Six => 225,
-        };
-        let mut amount = 0;
-        for date in DateRange(Utc::now(), until_date) {
-            if let chrono::Weekday::Mon = date.weekday() {
-                amount += per_weeks;
-            }
-        }
-        amount
-    }
-
-    fn get_bp_jades(use_bp: bool) -> i32 {
-        match use_bp {
-            true => 680,
-            false => 0,
-        }
-    }
-
-    pub fn get_rail_pass_jades(cfg: &RailPassCfg, diff_days: u32) -> i32 {
-        match cfg.days_left {
-            Some(days_left) if cfg.use_rail_pass => match days_left < diff_days {
-                true => 90 * diff_days as i32 + 300 * diff_days as i32 / 30,
-                false => 90 * diff_days as i32,
-            },
-            None if cfg.use_rail_pass => 90 * diff_days as i32,
-            _ => 0,
-        }
-    }
-
-    fn get_checkin_jades(until_date: DateTime<Utc>) -> i32 {
-        let mut amount: i32 = 0;
-        for date in DateRange(Utc::now(), until_date) {
-            if let 5 | 13 | 20 = date.day() {
-                amount += 20;
-            }
-        }
-        amount
-    }
-
-    fn get_character_trial_jades(patch_start: DateTime<Utc>, until_date: DateTime<Utc>) -> i32 {
-        // rewards from both banners in patch
-        match patch_start + Duration::weeks(3) < until_date {
-            true => 40,
-            false => 20,
-        }
-    }
-
-    fn get_monthly_ember_rolls(until_date: DateTime<Utc>) -> i32 {
-        let mut amount: i32 = 0;
-        for date in DateRange(Utc::now(), until_date) {
-            if let 1 = date.day() {
-                amount += 5;
-            }
-        }
-        amount
     }
 }
 

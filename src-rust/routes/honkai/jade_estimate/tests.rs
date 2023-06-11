4b189eb171c1b589e6587b27d852e8cea6ed0157
Othi <mnpq.raven@gmail.com>
2023-06-11 19:32:24 +0700

    minor date diffing bug fix

--- src-rust/routes/honkai/jade_estimate/tests.rs
+++ src-rust/routes/honkai/jade_estimate/tests.rs
@@ -6,12 +6,12 @@ use super::types::{RewardSourceType, Server};
 
 #[test]
 fn new_date_diffing() {
-    let to_date = Utc.with_ymd_and_hms(2023, 6, 30, 18, 29, 27).unwrap();
     let from_date = Utc.with_ymd_and_hms(2023, 6, 11, 2, 12, 12).unwrap();
+    let to_date = Utc.with_ymd_and_hms(2023, 6, 11, 19, 0, 0).unwrap();
     let days = RewardSourceType::Daily
         .get_difference(from_date, to_date, &Server::Asia)
         .unwrap();
-    assert_eq!(days, 20);
+    assert_eq!(days, 1);
 }
 
 #[test]

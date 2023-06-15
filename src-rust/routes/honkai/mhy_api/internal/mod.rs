4024eba6bbe54199e6f406d66e1dcba189937a69
Othi <mnpq.raven@gmail.com>
2023-06-15 20:35:48 +0700

    tmp write

--- src-rust/routes/honkai/mhy_api/internal/mod.rs
+++ src-rust/routes/honkai/mhy_api/internal/mod.rs
@@ -1,5 +1,5 @@
 use self::{categorizing::Character, constants::CHARACTER_DICT};
-use std::{collections::HashMap, error::Error};
+use std::{collections::HashMap, error::Error, fs};
 use tracing::debug;
 
 /// holds internal types for mhy's DB
@@ -11,8 +11,10 @@ mod runnables;
 
 // NOTE: url fetching
 pub async fn get_character_list() -> Result<Vec<Character>, Box<dyn Error>> {
-    debug!("fetching from fallback url");
-    let res_str: String = reqwest::get(CHARACTER_DICT).await?.text().await?;
-    let map: HashMap<String, Character> = serde_json::from_str(&res_str)?;
-    Ok(map.into_values().collect::<Vec<Character>>())
+    // let res_str: String = reqwest::get(CHARACTER_DICT).await?.text().await?;
+    // let map: HashMap<String, Character> = serde_json::from_str(&res_str)?;
+
+    let data = fs::read_to_string("/tmp/characters.json")?;
+    let t: Vec<Character> = serde_json::from_str(&data)?;
+    Ok(t)
 }

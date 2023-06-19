c453153a3f832d12cacdc12dfbb15bab4c19d9b2
Othi <mnpq.raven@gmail.com>
2023-06-19 16:38:25 +0700

    chore

--- api/cron/write_db.rs
+++ api/cron/write_db.rs
@@ -1,14 +1,15 @@
 use axum::Json;
 use nas_ws::handler::{error::WorkerError, FromAxumResponse};
-use nas_ws::routes::honkai::mhy_api::internal::write_character_db;
+use nas_ws::routes::honkai::mhy_api::internal::{
+    categorizing::DbCharacter, constants::CHARACTER_LOCAL, runnables::HasPath,
+};
 use response_derive::JsonResponse;
 use serde::Serialize;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
 #[derive(Debug, Clone, Serialize, JsonResponse)]
 struct ResponseData {
-    exist_status: bool,
-    write_status: bool,
+    character_db: bool,
 }
 
 #[tokio::main]
@@ -20,11 +21,10 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
-    let (exist_status, write_status) = write_character_db().await?;
+    let char_db = DbCharacter::try_write_disk(CHARACTER_LOCAL).await.is_ok();
 
     Ok(Json(ResponseData {
-        exist_status,
-        write_status,
+        character_db: char_db,
     }))
     .as_axum()
 }

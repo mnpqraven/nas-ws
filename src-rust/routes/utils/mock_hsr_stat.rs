a0b687d0bf05c51638726e238f42718b739acfeb
Othi <mnpq.raven@gmail.com>
2023-06-22 16:55:56 +0700

    wip

--- src-rust/routes/utils/mock_hsr_stat.rs
+++ src-rust/routes/utils/mock_hsr_stat.rs
@@ -1,14 +1,46 @@
+use crate::handler::{error::WorkerError, FromAxumResponse};
+use crate::routes::honkai::mhy_api::types_parsed::character::Character;
 use axum::Json;
+use fake::faker::name::raw::*;
+use fake::locales::*;
+use fake::Dummy;
 use fake::{Fake, Faker};
+use response_derive::JsonResponse;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 use tracing::instrument;
+use vercel_runtime::{Body, Response, StatusCode};
 
-use crate::{
-    handler::error::WorkerError, routes::honkai::mhy_api::types_parsed::character::Character,
-};
+#[derive(Debug, Deserialize, Serialize, JsonResponse, Clone, JsonSchema, Dummy)]
+pub struct MvpWrapper {
+    pub character: Character,
+    pub team_distribution: Vec<[InTeamDistribution; 4]>,
+    pub self_distribution: DamageSelfDistribution,
+}
+
+#[derive(Debug, Deserialize, Serialize, JsonResponse, Clone, JsonSchema, Dummy)]
+pub struct DamageSelfDistribution {
+    // % dist, count, avg.min, avg.max
+    #[dummy(faker = "((0.0..1.0), (1..100), (1000..10000), (10000..20000))")]
+    pub skill: (f32, u32, u32, u32),
+    #[dummy(faker = "((0.0..1.0), (1..100), (30000..40000), (40000..80000))")]
+    pub ult: (f32, u32, u32, u32),
+    #[dummy(faker = "((0.0..1.0), (1..100), (1000..2000), (2000..3000))")]
+    pub basic: (f32, u32, u32, u32),
+    #[dummy(faker = "((0.0..1.0), (1..100), (1000..10000), (10000..20000))")]
+    pub followup: (f32, u32, u32, u32),
+}
+#[derive(Debug, Deserialize, Serialize, JsonResponse, Clone, JsonSchema, Dummy)]
+pub struct InTeamDistribution {
+    #[dummy(faker = "Name(EN)")]
+    pub name: String,
+    #[dummy(faker = "0.0 .. 1.0")]
+    pub rate: f32,
+}
 
 #[instrument(ret, err)]
-pub async fn handle() -> Result<Json<Character>, WorkerError> {
-    let randomized_character: Character = Faker.fake();
+pub(super) async fn handle() -> Result<Json<MvpWrapper>, WorkerError> {
+    let randomized_character: MvpWrapper = Faker.fake();
 
     Ok(Json(randomized_character))
 }

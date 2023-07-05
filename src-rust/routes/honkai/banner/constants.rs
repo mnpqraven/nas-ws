47abf863406a130ead78d48ae8d46791a6b66da3
Othi <mnpq.raven@gmail.com>
2023-07-05 19:20:27 +0700

    fix banner

--- src-rust/routes/honkai/banner/constants.rs
+++ src-rust/routes/honkai/banner/constants.rs
@@ -1,6 +1,6 @@
 // char1, char2, version
 pub(super) const BANNER_CHARS: [(&str, Option<&str>, Option<&str> ); 3] = [
     ("1.1.0", Some("Silver Wolf"), Some("Luocha")),
-    ("1.2.0", Some("Silver Wolf"), Some("Kafka")),
+    ("1.2.0", Some("Blade"), Some("Kafka")),
     ("1.3.0", Some("Fu Xuan"), None),
 ];

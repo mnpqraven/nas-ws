2c009ed1d4f099082543980ac9d72a049eca276c
Othi <mnpq.raven@gmail.com>
2023-07-30 16:21:36 +0700

    base protobuf support

--- src-rust/routes/mod.rs
+++ src-rust/routes/mod.rs
@@ -1,6 +1,6 @@
 use self::{
     cron::write_db, dotfiles::dotfiles_routes, foo::foo_routes, honkai::honkai_routes,
-    utils::utils_routes,
+    rpc::rpc_routes, utils::utils_routes,
 };
 use axum::{routing::get, Router};
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
@@ -10,22 +10,19 @@ pub mod dotfiles;
 pub mod endpoint_types;
 mod foo;
 pub mod honkai;
+pub mod rpc;
 pub mod utils;
 
 pub fn app_router() -> Router {
     Router::new()
-        .route("/", get(root))
         .nest("/utils", utils_routes())
         .nest("/foo", foo_routes())
         .nest("/dotfiles", dotfiles_routes())
         .nest("/honkai", honkai_routes())
         .nest("/cron", cron_routes())
+        .nest("/rpc", rpc_routes())
         .layer(TraceLayer::new_for_http())
         .layer(CorsLayer::permissive())
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
 }
 
 fn cron_routes() -> Router {

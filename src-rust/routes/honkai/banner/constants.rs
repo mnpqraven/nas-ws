6ebc7639bdff832d60a638cf371400ae63591af3
Othi <mnpq.raven@gmail.com>
2023-08-21 08:39:13 +0700

    banner order

--- src-rust/routes/honkai/banner/constants.rs
+++ src-rust/routes/honkai/banner/constants.rs
@@ -2,10 +2,12 @@ use crate::handler::error::WorkerError;
 use serde::{Deserialize, Serialize};
 
 // char1, char2, version
-pub(super) const BANNER_CHARS: [(&str, Option<&str>, Option<&str>); 3] = [
+pub(super) const BANNER_CHARS: [(&str, Option<&str>, Option<&str>); 5] = [
+    ("1.0.0", Some("seele"), Some("jingyuan")),
     ("1.1.0", Some("silverwolf"), Some("luocha")),
     ("1.2.0", Some("blade"), Some("kafka")),
-    ("1.3.0", Some("fuxuan"), Some("danhengil")),
+    ("1.3.0", Some("danhengil"), Some("fuxuan")),
+    ("1.4.0", Some("jingliu"), Some("topaz")),
 ];
 
 #[derive(Debug, Serialize, Deserialize)]
